bd1e7b3ec117acf4feaba9c9fbebbf996ee9fe36
Sadko-o <ruana.saduakhas@nu.edu.kz>
2023-10-30 15:14:37 +0600

    newdesign not working

--- app/src/main/java/com/example/pktrcv/MainActivity.kt
+++ app/src/main/java/com/example/pktrcv/MainActivity.kt
@@ -9,11 +9,12 @@ import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import com.example.pktrcv.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -25,10 +26,13 @@ import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.lang.System.out
 import java.net.Socket
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import kotlin.reflect.typeOf
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -41,95 +45,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setInitialCardVisibility()
         listenForData()
     }
 
-    private fun setInitialCardVisibility() {
-        binding.firstCardView.visibility = View.GONE
-        binding.secondCardView.visibility = View.GONE
-        binding.thirdCardView.visibility = View.GONE
-    }
-
-//    private fun showAlert(title: String, message: String) {
-//        AlertDialog.Builder(this)
-//            .setTitle(title)
-//            .setMessage(message)
-//            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
-//            .show()
-//    }
-private fun showAlert(title: String, message: String) {
-    runOnUiThread {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle(title)
-        builder.setMessage(message)
-        builder.setPositiveButton("OK") { dialog, _ ->
-            dialog.dismiss()
+    private fun showAlert(title: String, message: String) {
+        runOnUiThread {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(title)
+            builder.setMessage(message)
+            builder.setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.show()
         }
-        builder.show()
     }
-}
 
     private fun listenForData() {
         coroutineScope.launch {
             while (isActive) {
                 val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
                 when {
-                    isConnectedToWifi(wifiManager) -> tryReceivingData(wifiManager)
+
+                    isConnectedToWifi(wifiManager) -> tryReceivingData()
                     else -> showWifiError()
                 }
-                delay(5000)
+                delay(3000)
             }
         }
     }
 
     private fun isConnectedToWifi(wifiManager: WifiManager): Boolean {
+        Toast.makeText(this, "isConnectedWifi", Toast.LENGTH_SHORT).show()
         return wifiManager.isWifiEnabled && wifiManager.connectionInfo.networkId != -1
     }
 
-    private suspend fun tryReceivingData(wifiManager: WifiManager) {
+    private suspend fun tryReceivingData() {
         try {
-            val ipAddressInt = wifiManager.connectionInfo.ipAddress
-            val currIpAddress = String.format(
-                "%d.%d.%d.%d",
-                ipAddressInt and 0xff,
-                ipAddressInt shr 8 and 0xff,
-                ipAddressInt shr 16 and 0xff,
-                ipAddressInt shr 24 and 0xff
-            )
-//            showAlert("Ip address", currIpAddress)
             val ipAddress = "192.168.4.1"
-//            https://192.168.4.1:8000/
             val port = 8000
-            Socket(ipAddress, port).use { socket ->
-                val receivedData = BufferedReader(InputStreamReader(socket.getInputStream())).readLine()
-                handleReceivedData(receivedData)
+            val receivedData: String? = withContext(Dispatchers.IO) {
+                val socket = Socket(ipAddress, port)
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+                val data = reader.readLine()
+                socket.close()
+                data
+            }
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, receivedData, Toast.LENGTH_SHORT).show()
+                handleReceivedData(receivedData ?: "")
             }
         } catch (e: Exception) {
+            Toast.makeText(this, "tryReceivedData", Toast.LENGTH_SHORT).show();
             showError(e.message ?: "Unknown Error")
         }
     }
 
 
     private suspend fun handleReceivedData(data: String) {
-        val jsonObject = JSONObject(data)
-        val packet = PacketData(
-            distance = jsonObject.getDouble("distance").toFloat(),
-            time = jsonObject.getInt("time"),
-            trainId = jsonObject.getInt("train_id"),
-            direction = jsonObject.getString("direction")[0],
-            timestamp = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
-        )
-        packetsList.add(packet)
+        try {
+            val jsonObject = JSONObject(data)
+            val packet = PacketData(
+                distance = jsonObject.getDouble("distance").toFloat(),
+                time = jsonObject.getInt("time"),
+                trainId = jsonObject.getInt("train_id"),
+                direction = jsonObject.getString("direction").first(),
+                timestamp = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+            )
+            packetsList.add(packet)
 
-        while (packetsList.size > 3) {
-            packetsList.removeAt(0)
+            while (packetsList.size > 3) {
+                packetsList.removeAt(0)
+            }
+
+            showAlert("received", packet.toString())
+            updateUI()
+            withContext(Dispatchers.Main) {
+                playNotificationSound()
+                vibrateDevice()
+            }
+        } catch (e: Exception) {
+            showError("Error parsing data: ${e.message}")
         }
-
-        updateUI()
     }
-
 
     private suspend fun showWifiError() {
         withContext(Dispatchers.Main) {
@@ -145,40 +143,37 @@ private fun showAlert(title: String, message: String) {
 
     private suspend fun updateUI() {
         withContext(Dispatchers.Main) {
-            binding.firstCardContainer.removeAllViews()
-
-            for (packet in packetsList) {
-                val packetBinding = inflatePacketLayout()
-                populatePacketView(packetBinding, packet)
-                binding.firstCardContainer.addView(packetBinding.root)
+            val containers = listOf(
+                binding.firstCardContainer,
+            )
+            for (index in packetsList.indices) {
+                populateLinearLayout(containers[index], packetsList[index], index+1)
             }
         }
     }
 
+    @SuppressLint("SetTextI18n", "DiscouragedApi")
+    private fun populateLinearLayout(linearLayout: LinearLayout, packet: PacketData, packetNumber: Int) {
+        val context = linearLayout.context
+        val textViewTrainId = linearLayout.findViewById<TextView>(
+            context.resources.getIdentifier("textViewTrainId$packetNumber", "id", context.packageName)
+        )
+        val textViewTimestamp = linearLayout.findViewById<TextView>(
+            context.resources.getIdentifier("textViewTimestamp$packetNumber", "id", context.packageName)
+        )
+        val textViewDistance = linearLayout.findViewById<TextView>(
+            context.resources.getIdentifier("textViewDistance$packetNumber", "id", context.packageName)
+        )
+        val textViewETA = linearLayout.findViewById<TextView>(
+            context.resources.getIdentifier("textViewETA$packetNumber", "id", context.packageName)
+        )
 
-
-    private fun inflatePacketLayout(): com.example.pktrcv.databinding.PacketLayoutBinding {
-        return com.example.pktrcv.databinding.PacketLayoutBinding.inflate(layoutInflater, binding.firstCardContainer, false)
+        textViewTrainId.text = "Train ID: ${packet.trainId}"
+        textViewTimestamp.text = "Received packet time: ${packet.timestamp}"
+        textViewDistance.text = "Distance: ${packet.distance} km"
+        textViewETA.text = "ETA: ${packet.time} minutes"
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun populatePacketView(packetBinding: com.example.pktrcv.databinding.PacketLayoutBinding, packet: PacketData) {
-        packetBinding.textViewTrainId.text = "Train ID: ${packet.trainId}"
-        packetBinding.textViewTimestamp.text = "Received packet time: ${packet.timestamp}"
-        packetBinding.textViewDistance.text = "Distance: ${packet.distance} km"
-        packetBinding.textViewETA.text = "ETA: ${packet.time} minutes"
-    }
-
-
-
-    @SuppressLint("SetTextI18n")
-    private fun updateCardData(cardView: CardView, packet: PacketData) {
-        val linearLayout = cardView.getChildAt(0) as LinearLayout
-        (linearLayout.getChildAt(0) as TextView).text = "Received packet time: ${packet.timestamp}"
-        (linearLayout.getChildAt(1) as TextView).text = "Train ID:             ${packet.trainId}"
-        (linearLayout.getChildAt(2) as TextView).text = "Distance:             ${packet.distance}"
-        (linearLayout.getChildAt(3) as TextView).text = "ETA:                  ${packet.time} minutes"
-    }
 
     private fun playNotificationSound() {
         try {
@@ -194,7 +189,7 @@ private fun showAlert(title: String, message: String) {
         val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
         if (vibrator?.hasVibrator() == true) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vibrator.vibrate(VibrationEffect.createOneShot(5000, VibrationEffect.DEFAULT_AMPLITUDE))
+                vibrator.vibrate(VibrationEffect.createOneShot(2000, VibrationEffect.DEFAULT_AMPLITUDE))
             } else {
                 @Suppress("DEPRECATION")
                 vibrator.vibrate(5000)
